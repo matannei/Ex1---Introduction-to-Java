cf84028e7fcf59800ada8d7c1f4f078a587fd04b
matannei <73829806+matannei@users.noreply.github.com>
2020-11-13 01:29:57 +0200

    Add files via upload

--- Ex1.java
+++ Ex1.java
@@ -1,90 +1,91 @@
-package ex1;
-
+package Ex1;
 import java.util.Scanner;
 
 public class Ex1 {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
+		int num1, num2;
+		if (args.length > 0) {
+			num1 = Integer.parseInt(args[0]);
+			num2 = Integer.parseInt(args[1]);
+		} 
+		
+		else {
 
-        int num1, num2;
-        if (args.length > 0) {
-            num1 = Integer.parseInt(args[0]);
-            num2 = Integer.parseInt(args[1]);
-        } else {
+			Scanner scanner = new Scanner(System.in);
+			//int num1, num2,t=1;
+			System.out.println("Enter the first number for max prime GCD: ");
+			num1 = scanner.nextInt();
+			System.out.println("Enter the second number for max prime GCD: ");
+			num2 = scanner.nextInt();
 
+			scanner.close();
 
-            Scanner scanner = new Scanner(System.in);
-            //int num1, num2,t=1;
-            System.out.println("please enter your first number");
-            num1 = scanner.nextInt();
-            System.out.println("please enter your second number");
-            num2 = scanner.nextInt();
-
-
-            scanner.close();
-
-        }
+		}
 
 
-        int t = 1;
-        if (num2 < num1) {
-            int i = num2;
+		int t = 1;
+		if (num2 < num1) {
+			int i = num2;
 
-            while (i > 1) {
+			while (i > 1) {
 
-                if (num1 % i == 0 && num2 % i == 0) {
-                    t = i;
-                    boolean prime = true;
-                    int j = 2;
-                    while (j <= Math.sqrt(t)) {
-                        if (t % j == 0) {
-                            prime = false;
-                        }
-                        j = j + 1;
+				if (num1 % i == 0 && num2 % i == 0) {
+					t = i;
+					boolean prime = true;
+					int j = 2;
+					while (j <= Math.sqrt(t)) {
+						if (t % j == 0) {
+							prime = false;
+						}
+						j = j + 1;
 
-                    }
-                    if (prime) {
-                        System.out.println("answer is " + i);
-                        return;
-                    }
-                }
+					}
+					if (prime) {
+						System.out.println("Computes the GPCD("+num1+","+num2+") =");
+						System.out.println("Max Prime Common Divider: " + i);
+						return;
+					}
+				}
 
-                i = i - 1;
+				i = i - 1;
 
-            }}
+			}}
 
-        else {
-            int i = num1;
+		else {
+			int i = num1;
 
-            while (i > 1) {
+			while (i > 1) {
 
-                if (num1 % i == 0 && num2 % i == 0) {
-                    t = i;
-                    boolean prime = true;
-                    int j = 2;
-                    while (j <= Math.sqrt(t)) {
-                        if (t % j == 0) {
-                            prime = false;
-                        }
-                        j = j + 1;
+				if (num1 % i == 0 && num2 % i == 0) {
+					t = i;
+					boolean prime = true;
+					int j = 2;
+					while (j <= Math.sqrt(t)) {
+						if (t % j == 0) {
+							prime = false;
+						}
+						j = j + 1;
 
-                    }
-                    if (prime) {
-                        System.out.println("answer is " + i);
-                        return;
-                    }
-                }
+					}
+					if (prime) {
+						System.out.println("Computes the GPCD("+num1+","+num2+") =");
+						System.out.println("Max Prime Common Divider: " + i);
 
-                i = i - 1;
+						return;
+					}
+				}
 
-            }
+				i = i - 1;
 
-        }
-        System.out.println("answer is " + t);
+			}
 
-    }
+		}
+		System.out.println("Computes the GPCD("+num1+","+num2+") =");
+		System.out.println("Max Prime Common Divider: " + t);
+
+	}
 }
-
 
 
